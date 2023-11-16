e586f7e5a5484a41619603023e31e6d3553fcac3
Rakhi Kumari <rakhi.kumari@elucidata.io>
2023-11-16 13:56:17 +0530

    adding nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,6 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatSidenavModule} from "@angular/material/sidenav";
-import {MatIconModule}    from "@angular/material/icon";
-import {MatListModule} from "@angular/material/list";
-import {MatButtonModule} from "@angular/material/button";
-import { MatTabsModule } from '@angular/material/tabs';
-
 
 @NgModule({
   declarations: [
@@ -19,12 +12,6 @@ import { MatTabsModule } from '@angular/material/tabs';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatButtonModule,
-    MatTabsModule
   ],
   providers: [
     provideClientHydration()
